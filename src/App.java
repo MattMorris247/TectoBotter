69c87b81b069afa512e9f72f24512dc954043231
Matt Morris <73907438+MattMorris247@users.noreply.github.com>
2022-09-09 01:47:26 -0400

    BUNCH OF PROGRESS

--- src/App.java
+++ src/App.java
@@ -6,26 +6,50 @@ import java.io.FileReader;
 import java.util.Iterator;
 import java.util.Map;
 import java.io.FileNotFoundException;
-
+import java.util.ArrayList;
 import java.io.IOException;
-  
+
 public class App {
     @SuppressWarnings("unchecked")
-    public static void main(String[] args) throws Exception { 
-          //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\033[0m";
+    public static final String YELLOW = "\u001B[33m";
 
-        try (FileReader reader = new FileReader("src/OldData.json"))
-        {
-            //Read JSON file
+    public static ArrayList<Member> OldMemberList = new ArrayList<Member>();
+    public static ArrayList<Member> NewMemberList = new ArrayList<Member>();
+    public static JSONParser jsonParser = new JSONParser();
+
+    public static void main(String[] args) throws Exception {
+        // JSON parser object to parse read file
+        createOldDataList();
+        createNewDataList();
+
+        System.out.println(OldMemberList);
+        System.out.println(NewMemberList);
+
+    }
+
+    private static void createOldDataList() {
+        try (FileReader reader = new FileReader("src/OldData.json")) {
+            // Read JSON file
             Object obj = jsonParser.parse(reader);
- 
             JSONArray playerList = (JSONArray) obj;
-            System.out.println(playerList);
-             
-            //Iterate over employee array
-            playerList.forEach( play -> parsePlayerObject( (JSONObject) play ) );
- 
+            for (int i = 0; i < 5; i++) {
+                // Parsing RSN
+                String player = playerList.get(i).toString();
+                int rsnIndex = player.indexOf("username") + 11;
+                String str = player.substring(rsnIndex, player.length());
+                String rsn = rsnTrimmer(str);
+                // PARSING EXP GAINED (END DATE)
+                int newIndex = player.indexOf("gained") + 8;
+                int startIndex = player.indexOf("start");
+                String str2 = player.substring(newIndex, startIndex);
+                String gained = gainTrimmer(str2);
+                int ehpGain = Integer.parseInt(gained);
+                Member mem = new Member(rsn, ehpGain, false);
+                OldMemberList.add(mem);
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -33,20 +57,49 @@ public class App {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
     }
-    private static void parsePlayerObject(JSONObject player) 
-    {
-        //Get employee object within list
-        JSONObject playerObject = (JSONObject) player.get("player");
-         
-        //Get employee first name
-        String rsn= (String) playerObject.get("name");    
-        System.out.println(rsn);
-        
+
+    private static void createNewDataList() {
+        try (FileReader reader = new FileReader("src/OldData.json")) {
+            // Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray playerList = (JSONArray) obj;
+            for (int i = 0; i < 5; i++) {
+                // Parsing RSN
+                String player = playerList.get(i).toString();
+                int rsnIndex = player.indexOf("username") + 11;
+                String str = player.substring(rsnIndex, player.length());
+                String rsn = rsnTrimmer(str);
+                // PARSING EXP GAINED (END DATE)
+                int newIndex = player.indexOf("gained") + 8;
+                int startIndex = player.indexOf("start");
+                String str2 = player.substring(newIndex, startIndex);
+                String gained = gainTrimmer(str2);
+                int ehpGain = Integer.parseInt(gained) + 5;
+                Member mem = new Member(rsn, ehpGain, true);
+                NewMemberList.add(mem);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String rsnTrimmer(String str) {
+        String rsn = str.substring(0, str.length() - 2);
+        return rsn;
+    }
+
+    private static String gainTrimmer(String str) {
+
+        String gained = str.substring(0, str.indexOf("."));
+        return gained;
     }
 }
-        
-
-
-         
-   
---
+++ src/Member.java
@@ -0,0 +1,43 @@
+public class Member {
+    private String rsn;
+    private int ehp;
+    private boolean type;
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\033[0m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String PURP = "\u001B[35m";
+
+    public Member(String username, int gained, boolean typer) {
+        rsn = username;
+        ehp = gained;
+        type = typer;
+    }
+
+    public String toString() {
+        if (!type) {
+            return (BLUE + rsn + RESET + " " + YELLOW + ehp + RESET);
+        } else {
+            return (PURP + rsn + RESET + " " + YELLOW + ehp + RESET);
+        }
+    }
+
+    private String getUser() {
+        return this.rsn;
+    }
+
+    public int compare(Member mem1, Member mem2) {
+        String user1 = mem1.getUser();
+        String user2 = mem2.getUser();
+
+        if (user1.equals(user2)) {
+            return 0;
+        }
+        if (user1.compareTo(user2) < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+
+    }
+
+}
